609e2629152029f79772259bd827151dc9bdf372
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-09 13:52:47 +0600

    Area of a retangle using structure in C

--- area_of_rectangle.c
+++ area_of_rectangle.c
@@ -1,17 +1,26 @@
+/**
+ *    author: mohin    
+**/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 struct rectangle {
     double length;
     double breadth;
+    double area;
 };
 
 int main() {
     struct rectangle r;
-    printf("Enter length of the rectangle : ");
+    printf("length of the rectangle : ");
     scanf("%lf", &r.length);
-    printf("Enter breadth of the rectangle : ");
+    printf("breadth of the rectangle : ");
     scanf("%lf", &r.breadth);
-    printf("Length = %0.2lf Breadth = %0.2lf\n", r.length, r.breadth);
-    printf("Area = %0.2lf\n", r.length * r.breadth);
+    r.area = r.length * r.breadth;
+    printf("length = %0.2lf\nbreadth = %0.2lf\n", r.length, r.breadth);
+    printf("area = %0.2lf unit\n", r.area);
     return 0;
 }
