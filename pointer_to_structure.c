50489b11e0a67a659d71b535b158806b7faa3960
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-10 19:11:31 +0600

    Pointer to Structure in C

--- pointer_to_structure.c
+++ pointer_to_structure.c
@@ -7,19 +7,20 @@
 #include <string.h>
 #include <math.h>
 
-struct rectangle {
+struct Rectangle {
     double length;
     double breadth;
     double area;
 };
 
 int main() {
-    struct rectangle r;
+    struct Rectangle r;
     r.length = 100;
     r.breadth = 100;
     r.area = r.length * r.breadth;
-    struct rectangle *p; // declaration
-    p = &r; // initialization
+
+    struct Rectangle *p; // Declaration
+    p = &r; // Initialization
     printf("sizeof (struct rectangle) = %d\n", sizeof(p));
     printf("length = %0.2lf\n", p->length);
     printf("breadth = %0.2lf\n", p->breadth);
@@ -27,17 +28,20 @@ int main() {
     printf("length = %0.2lf\n", (*p).length);
     printf("breadth = %0.2lf\n", (*p).breadth);
     printf("area = %0.2lf\n", (*p).area);
-    p = (struct rectangle *)malloc(sizeof(struct rectangle)); 
-    // before initialization
+
+    p = (struct Rectangle *)malloc(sizeof(struct Rectangle)); 
+    // Before initialization
     printf("length = %0.2lf\n", p->length); // 0.00
     printf("breadth = %0.2lf\n", p->breadth); // 0.00
     printf("area = %0.2lf\n", p->area); // 0.00
-    // after initializing with some value
+
+    // After initializing with some value
     p->length = 25.25;
     p->breadth = 12.625;
     p->area = p->length * p->breadth;
     printf("length = %0.3lf\n", p->length);
     printf("breadth = %0.3lf\n", p->breadth);
     printf("area = %0.3lf\n", p->area);
+
     return 0;
 }
