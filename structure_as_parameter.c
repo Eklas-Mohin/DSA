2a67e5bd71b1e42b6def867d51449f0c0311f9e0
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-10 19:02:19 +0600

    Structures as parameter in C

--- structure_as_parameter.c
+++ structure_as_parameter.c
@@ -7,46 +7,51 @@
 #include <string.h>
 #include <math.h>
 
-struct student {
+struct Student {
     char name[25];
     int roll;
     int rank;
     char section;
 };
 
-void print_details(struct student data) {
+void print_details(struct Student data) {
     printf("Name: %s", data.name);
     printf("     Roll: %03d", data.roll);
     printf("     Rank: %03d", data.rank);
     printf("     Section: %c\n", data.section);
 }
 
-void change_section(struct student *data, char ch) {
-    data->section = ch; // dereferencing
+void change_section(struct Student *data, char ch) {
+    data->section = ch; // Dereferencing
 }
 
-struct student* heap_memory() {
-    struct student *ptr;
-    ptr = (struct student *)malloc(sizeof(struct student));
+struct Student* heap_memory() {
+    struct Student *ptr;
+
+    ptr = (struct Student *)malloc(sizeof(struct Student));
     if (ptr == NULL) {
         printf("Memory allocation failed\n");
         exit(1);
     }
+
     strcpy(ptr->name, "Eklas Mohin");
     ptr->roll = 112;
     ptr->rank = 1;
     ptr->section = 'C';
+    
     return ptr;
 }
 
 int main() {
-    struct student s1 = {"Eklas Mohin", 112, 1, 'A'};
-    print_details(s1); // call by value
-    change_section(&s1, 'C'); // call by reference --> s1
+    struct Student s1 = {"Eklas Mohin", 112, 1, 'A'};
+
+    print_details(s1); // Call by value
+    change_section(&s1, 'C'); // Call by reference --> s1
     print_details(s1);
-    struct student *ptr = heap_memory();
+
+    struct Student *ptr = heap_memory();
     print_details(*ptr);
-    // Free the allocated memory
-    free(ptr);
+    free(ptr); // Free the allocated memory
+
     return 0;
 }
