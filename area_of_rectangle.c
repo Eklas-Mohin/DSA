26dcc98379cf8ee72185cb43a11d1a716c5ae31d
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-10 19:40:03 +0600

    Area of a rectangle using Structure in C

--- area_of_rectangle.c
+++ area_of_rectangle.c
@@ -15,12 +15,15 @@ struct Rectangle {
 
 int main() {
     struct Rectangle r;
+
     printf("Enter length of the rectangle : ");
     scanf("%lf", &r.length);
     printf("Enter breadth of the rectangle : ");
     scanf("%lf", &r.breadth);
+
     r.area = r.length * r.breadth;
     printf("Length = %0.2lf\nBreadth = %0.2lf\n", r.length, r.breadth);
     printf("Area = %0.2lf unit\n", r.area);
+    
     return 0;
 }
