e1ffb123604012f27a07e9fc40fd6660697847e4
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-10 13:09:37 +0600

    Array as parameter in C++

---
+++ array_passing.c
@@ -0,0 +1,34 @@
+/**
+ *    author: mohin    
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void mod_array(int arr[], int len) {
+    for (int i = 0; i < len; ++i) {
+        *(arr + i) = *(arr + i) * 5; 
+        // original array is modified
+    }
+}
+
+int main() {
+    int arr[] = {1, 5, 25, 125, 625, 3125};
+    int len = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < len; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    mod_array(arr, len);
+    for (int i = 0; i < len; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+/*
+ arrays are only passed by reference
+*/
