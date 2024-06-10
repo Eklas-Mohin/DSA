f275342ef7f914cc4db598e4659f2f1b6af231bd
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-10 19:16:58 +0600

    Functions in C

--- functions.c
+++ functions.c
@@ -7,22 +7,23 @@
 #include <string.h>
 #include <math.h>
 
-int add(int a, int b) { // formal parameters
+int add(int a, int b) { // Formal parameters
     int c = a + b;
     return c;
 }
 
 int main() {
     int num1 = 10, num2 = 101;
-    int sum = add(num1, num2); // actual parameters
+    int sum = add(num1, num2); // Actual parameters
     printf("sum = %d\n", sum);
     return 0;
 }
 
 /*
-    all the codes are written inside the 
+    All the codes are written inside the 
     main function --> monolithic programming
-    breaking a program into smaller functions
+
+    Breaking a program into smaller functions
     then integrating those functions inside
     the main function --> modular programming or
     procedural programming
