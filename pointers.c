a380757c6b55a35a134c5093541dddfe18bacf91
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-10 19:06:44 +0600

    Pointers in C

--- pointers.c
+++ pointers.c
@@ -20,24 +20,27 @@ void pointer_size() {
     float *p3;
     double *p4;
     struct demo *p5;
+
+    // Size of a pointer is independent of its data type
     printf("int <> sizeof(ptr) = %d\n", sizeof(p1)); // 8
     printf("char <> sizeof(ptr) = %d\n", sizeof(p2)); // 8
     printf("float <> sizeof(ptr) = %d\n", sizeof(p3)); // 8
     printf("double <> sizeof(ptr) = %d\n", sizeof(p4)); // 8
     printf("struct demo <> sizeof(ptr) = %d\n\n", sizeof(p5)); // 8
-    // size of a pointer is independent of its data type
 }
 
 int main() {
     int a = 10;
-    int *p; // declaration
-    p = &a; // initialization
+    int *p; // Declaration of a pointer of type integer
+    p = &a; // Initialization
+
     printf("%d\n", a);
-    printf("%d\n", *p); // dereferencing
-    printf("%p\n", p); // memory address
-    printf("%p\n", &a); // memory address
+    printf("%d\n", *p); // Dereferencing
+    printf("%p\n", p); // Memory address
+    printf("%p\n", &a); // Memory address
+
     int *heap, len = 5;
-    heap = (int *)malloc(len * sizeof(int)); // created in heap memory
+    heap = (int *)malloc(len * sizeof(int)); // Created in heap memory
     for (int i = 0; i < len; ++i) {
         heap[i] = i * 2 + 1;
     }
@@ -45,13 +48,16 @@ int main() {
         printf("%d ", *(heap + i));
     }
     printf("\n");
-    free(heap); // deallocation
+    free(heap); // Free the allocated memory
+
     int arr[] = {2, 4, 6, 8, 10};
     int *ptr = arr; // arr = &arr[0]
     for (int i = 0; i < (sizeof(arr) / sizeof(int)); ++i) {
         printf("%d ", ptr[i]);
     }
     printf("\n");
+
     pointer_size();
+
     return 0;
 }
