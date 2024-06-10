178a9ed71a23498d9d78d3fa72546984ba28806c
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-10 19:04:55 +0600

    Reference variables in C++

--- reference_variables.cpp
+++ reference_variables.cpp
@@ -9,22 +9,22 @@ using namespace std;
 int main() {
     int x = 10;
     int &r = x; // r is alias of x
-    /*
-    reference variables must be 
-    initialized during declaration
-    */ 
+    // Reference variables must be initialized during declaration
+
     cout << "x = " << x << endl;
     cout << "r = " << r << endl;
-    r += 1; // x += 1
-    // r and x has same memory address
+
+    // x and r has same memory address
+    cout << "&x = " << &x << endl;
+    cout << "&r = " << &r << endl;
+    r += 3;
     cout << "x = " << x << endl;
     cout << "r = " << r << endl;
-    x += 2;
-    cout << "x = " << x << endl;
-    cout << "r = " << r << endl;
+
     int y = 100;
     r = y;
     cout << "x = " << x << endl;
     cout << "r = " << r << endl;
+
     return 0;
 }
