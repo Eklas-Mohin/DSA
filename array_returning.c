eee5d847730cdc60a59231eadc56c205e6db93df
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-10 13:10:07 +0600

    Array as parameter in C++

---
+++ array_returning.c
@@ -0,0 +1,29 @@
+/**
+ *    author: mohin    
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int* array(int len) {
+    int *p;
+    p = (int *)malloc(len * sizeof(int));
+    for (int i = 0; i < len; ++i) {
+        p[i] = i * 5;
+    }
+    return p;
+}
+
+int main() {
+    int len;
+    printf("length of the array : ");
+    scanf("%d", &len);
+    int *heap_arr = array(len);
+    for (int i = 0; i < len; ++i) {
+        printf("%d ", heap_arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
