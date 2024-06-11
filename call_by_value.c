48bfb56848c45c8e770a9f3b26799839195f8c0d
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-11 07:15:50 +0600

    Call by value in C

--- call_by_value.c
+++ call_by_value.c
@@ -11,15 +11,14 @@ void swap(int a, int b) { // Call by value
     int temp = a;
     a = b;
     b = temp; // Formal parameters are modified
-    printf("swap function <> a = %d   b = %d \n", a, b);
-    
+    printf("Swap function <> a = %d   b = %d \n", a, b);
 }
 
 int main() {
     int a = 15, b = 51;
-    printf("before calling swap function <> a = %d   b = %d \n", a, b);
-    swap(a, b); // Actual parameters remain same
-    printf("after calling swap function <> a = %d   b = %d \n", a, b);
+    printf("Before calling swap function <> a = %d   b = %d \n", a, b);
+    swap(a, b); // Actual parameters remains the same
+    printf("After calling swap function <> a = %d   b = %d \n", a, b);
     return 0;
 }
 
