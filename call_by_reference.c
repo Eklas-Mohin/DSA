bf0b163a620de652dbc0f59f4a162b399c1bc533
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-11 10:06:50 +0600

    Call by reference in C

--- call_by_reference.c
+++ call_by_reference.c
@@ -7,21 +7,21 @@
 #include <string.h>
 #include <math.h>
 
-void swap(int *a, int *b) { // Call by reference
+void swapFunction(int *a, int *b) { 
     int temp = *a;
     *a = *b;
-    *b = temp; // Formal parameters are modified
+    *b = temp;  // Actual parameters are modified
     printf("Swap function <> a = %d   b = %d \n", *a, *b);
 }
 
 int main() {
     int a = 15, b = 51;
+
     printf("Before calling swap function <> a = %d   b = %d \n", a, b);
-    swap(&a, &b); // Actual parameters are modified
+    swapFunction(&a, &b); // Call by reference
     printf("After calling swap function <> a = %d   b = %d \n", a, b);
+
     return 0;
 }
 
-/*
-    Call by reference == Pass by reference
-*/
+// Call by reference == Pass by reference
