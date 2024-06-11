8b6997c75b57bf42e56948e473430a73b6cd27db
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-11 07:21:49 +0600

    Pass by reference in C++

--- pass_by_reference.cpp
+++ pass_by_reference.cpp
@@ -10,13 +10,13 @@ void swap_num(int &a, int &b) { // Call by reference
     int temp = a;
     a = b;
     b = temp;
-    cout << "swap function <> a = "<< a << "  b = " << b << endl;
+    cout << "Swap function <> a = "<< a << "  b = " << b << endl;
 }
 
 int main() {
     int a = 15, b = 51;
-    cout << "before swapping <> a = "<< a << "  b = " << b << endl; 
+    cout << "Before swapping <> a = "<< a << "  b = " << b << endl; 
     swap_num(a, b);
-    cout << "after swapping <> a = "<< a << "  b = " << b << endl;
+    cout << "After swapping <> a = "<< a << "  b = " << b << endl;
     return 0;
 }
