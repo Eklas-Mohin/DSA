efac23802464427e85b4b948d17b2d6aca36afcf
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-11 10:44:59 +0600

    Returning array in C

--- returning_array.c
+++ returning_array.c
@@ -7,23 +7,29 @@
 #include <string.h>
 #include <math.h>
 
-int* array(int len) {
+int* heapArray(int len) {
     int *p;
+
     p = (int *)malloc(len * sizeof(int));
-    for (int i = 0; i < len; ++i) {
-        p[i] = i * 5;
+    if (p == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
     }
+    for (int i = 0; i < len; ++i)  p[i] = i * 5;
+
     return p;
 }
 
 int main() {
     int len;
+
     printf("Enter length of the array : ");
     scanf("%d", &len);
-    int *heap_arr = array(len);
+    int *heap_arr = heapArray(len);
     for (int i = 0; i < len; ++i) {
         printf("%d ", heap_arr[i]);
     }
     printf("\n");
+
     return 0;
 }
