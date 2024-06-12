95efce1f539edbc25ba2784c772b858d71f97de8
Eklas-Mohin <mohin.030299@gmail.com>
2024-06-12 14:41:19 +0600

    Class and Constructor example in C++

--- class_and_constructor.cpp
+++ class_and_constructor.cpp
@@ -13,25 +13,39 @@ private:
     double area;
 
 public:
-    Rectangle(double l, double b) {
+    Rectangle() { // Default constructor
+        length = 0;
+        breadth = 0;
+    }
+    Rectangle(double l, double b) { // Parameterized constructor
         length = l;
         breadth = b;
     }
     void areaOfTheRectangle() {
         area = length * breadth;
-        cout <<"Area = " << area << endl;
+        cout << "Area = " << area << endl;
     }
     void changeLength(double l) {
         length = l;
     }
+    void setRectangle(double l, double b) {
+        length = l;
+        breadth = b;
+    }
 };
 
 int main() {
-    Rectangle r(16, 15);
+    Rectangle r1(16, 15);
 
-    r.areaOfTheRectangle();
-    r.changeLength(20);
-    r.areaOfTheRectangle();
+    r1.areaOfTheRectangle();
+    r1.changeLength(20);
+    r1.areaOfTheRectangle();
+
+    Rectangle r2;
+
+    r2.areaOfTheRectangle();
+    r2.setRectangle(12, 12);
+    r2.areaOfTheRectangle();
 
     return 0;
 }
