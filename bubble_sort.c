cd243334fe7ddb328004b71170f9b162df31e2cc
Eklas-Mohin <mohin030299@gmail.com>
2024-10-16 14:31:21 +0600

    Basic Sorting Algorithm - Bubble Sort

---
+++ bubble_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int main() {
+    int arr[] = {17, 23, 7, 59, 73, 13, 37, 53, 47};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted Array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Bubble Sort algorithm
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+
+    printf("\nSorted Array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
