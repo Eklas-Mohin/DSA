578acae294a7096bab978e6e5a6243e5acd879dc
Eklas-Mohin <mohin030299@gmail.com>
2024-10-17 17:19:38 +0600

    Basic Sorting Algorithm - Bubble Sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
+#include <float.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <errno.h>
+#include <assert.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <locale.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <pthread.h>
 
 void swap(int *xp, int *yp) {
     int temp = *xp;
@@ -6,20 +28,17 @@ void swap(int *xp, int *yp) {
     *yp = temp;
 }
 
-int main() {
-    int arr[] = {17, 23, 7, 59, 73, 13, 37, 53, 47};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Unsorted Array:\n");
-    for (int i = 0; i < n; i++) {
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
 
-    // Bubble Sort algorithm
-    for (int i = 0; i < n - 1; i++) {
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
         int swaps = 0;
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(&arr[j], &arr[j + 1]);
                 swaps += 1;
@@ -27,12 +46,19 @@ int main() {
         }
         if (!swaps) break;
     }
+}
+
+int main() {
+    int arr[] = {17, 23, 7, 59, 73, 13, 37, 53, 47};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted Array:\n");
+    printArray(arr, size);
+
+    bubbleSort(arr, size);
 
     printf("\nSorted Array:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    printArray(arr, size);
 
     return 0;
 }
