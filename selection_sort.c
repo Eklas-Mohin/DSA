0fd14976282f9f44d4ebbc9a6f50fbed51695947
Eklas-Mohin <mohin030299@gmail.com>
2024-10-17 15:10:49 +0600

    Basic Sorting Algorithm - Selection Sort

---
+++ selection_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {17, 23, 7, 59, 73, 13, 37, 53, 47};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted Array:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < n - 1; ++i) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+    
+    printf("\nSorted Array:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
